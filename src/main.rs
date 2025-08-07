e3b6f7187ce47a9513f28c3eda5e15da93f4c0f9
John Schoeman <johnschoeman1617@gmail.com>
2025-08-07 19:35:34 -0400

    Add logic for current player and active cell state

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use color_eyre::Result;
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
 use ratatui::{
+    DefaultTerminal, Frame,
     buffer::Buffer,
     layout::Rect,
-    style::Stylize,
-    symbols::border,
-    text::{Line, Text},
-    widgets::{Block, Borders, Paragraph, Widget},
     prelude::{Constraint, Direction, Layout},
-    DefaultTerminal, Frame,
+    style::{Color, Style},
+    style::{Modifier, Stylize},
+    symbols::border,
+    text::{Line, Span},
+    widgets::{Block, BorderType, Borders, Paragraph, Widget},
 };
+use std::fmt;
 
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
@@ -20,28 +22,36 @@ fn main() -> color_eyre::Result<()> {
 }
 
 #[derive(Debug, Default)]
-enum Row {
-    #[default] R0,
+enum RowIdx {
+    #[default]
+    R0,
     R1,
     R2,
 }
 
 #[derive(Debug, Default)]
-enum Col {
-    #[default] C0,
+enum ColIdx {
+    #[default]
+    C0,
     C1,
     C2,
 }
 
-type CellPos = ( Row, Col );
+type Coordinate = (usize, usize);
 
 #[derive(Debug, Default, Clone)]
 enum Player {
-    #[default] X,
+    #[default]
+    X,
     O,
 }
-fn show_player(player: Option<Player>) -> String {
-    "X".to_string()
+impl fmt::Display for Player {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Player::X => write!(f, "X"),
+            Player::O => write!(f, "O"),
+        }
+    }
 }
 
 enum Dir {
@@ -52,11 +62,97 @@ enum Dir {
 }
 
 #[derive(Debug, Default)]
+enum Col {
+    #[default]
+    N,
+    X,
+    O,
+}
+type Row = (Col, Col, Col);
+type Board = (Row, Row, Row);
+
+struct Ui {}
+
+#[derive(Debug, Default)]
 pub struct App {
-    current_player: Player,
-    board: Vec<Vec<Cell>>,
-    active_cell: CellPos,
+    game_state: GameState,
     exit: bool,
+}
+
+#[derive(Debug, Default)]
+pub struct GameState {
+    current_player: Player,
+    board: Board,
+    active_cell: Coordinate,
+}
+
+// #[derive(Debug, Default)]
+// struct Cell {
+//     app
+//     owner: Option<Player>,
+//     is_active: bool,
+// }
+
+struct Cell<'game_state> {
+    game_state: &'game_state GameState,
+    row: usize,
+    col: usize,
+}
+
+impl<'game_state> Cell<'game_state> {
+    fn new(game_state: &'game_state GameState, row: usize, col: usize) -> Self {
+        Self {
+            game_state,
+            row,
+            col,
+        }
+    }
+}
+
+impl GameState {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn cell(&self, (r, c): Coordinate) -> Cell {
+        Cell::new(self, r, c)
+    }
+
+    fn move_active_cell(&mut self, direction: Dir) {
+        match direction {
+            Dir::Left => {
+                if (self.active_cell.1 > 0) {
+                    let next_active_cell = (self.active_cell.0, self.active_cell.1 - 1);
+                    self.active_cell = next_active_cell;
+                }
+            }
+            Dir::Right => {
+                if (self.active_cell.1 < 2) {
+                    let next_active_cell = (self.active_cell.0, self.active_cell.1 + 1);
+                    self.active_cell = next_active_cell;
+                }
+            }
+            Dir::Up => {
+                if (self.active_cell.0 > 0) {
+                    let next_active_cell = (self.active_cell.0 - 1, self.active_cell.1);
+                    self.active_cell = next_active_cell;
+                }
+            }
+            Dir::Down => {
+                if (self.active_cell.0 < 2) {
+                    let next_active_cell = (self.active_cell.0 + 1, self.active_cell.1);
+                    self.active_cell = next_active_cell;
+                }
+            }
+        }
+    }
+
+    fn toggle_current_player(&mut self) {
+        match self.current_player {
+            Player::O => self.current_player = Player::X,
+            Player::X => self.current_player = Player::O,
+        }
+    }
 }
 
 impl App {
@@ -73,23 +169,44 @@ impl App {
     }
 
     fn draw(&self, frame: &mut Frame) {
+        let terminal_rect = frame.area();
+
+        let outer_block = Block::default()
+            .borders(Borders::ALL)
+            .title(Span::styled(
+                "Tic Tac Tui",
+                Style::default()
+                    .fg(Color::LightYellow)
+                    .add_modifier(Modifier::BOLD),
+            ))
+            .border_type(BorderType::Rounded);
+        frame.render_widget(outer_block, terminal_rect);
+
         let outer_layout = Layout::default()
             .direction(Direction::Vertical)
-            .constraints(vec![
-                Constraint::Percentage(80),
-                Constraint::Percentage(20),
-            ])
-            .split(frame.area());
-
-
+            .vertical_margin(1)
+            .horizontal_margin(1)
+            .constraints(vec![Constraint::Percentage(80), Constraint::Percentage(20)])
+            .split(terminal_rect);
 
         // frame.render_widget(self, frame.area());
         frame.render_widget(self, outer_layout[0]);
 
+        let current_player_text = self.game_state.current_player.to_string();
+        let active_cell_row_text = self.game_state.active_cell.0.to_string();
+        let active_cell_col_text = self.game_state.active_cell.1.to_string();
         frame.render_widget(
-            Paragraph::new("Bottom")
-                .block(Block::new().borders(Borders::ALL)),
-            outer_layout[1]);
+            Paragraph::new(Line::from_iter([
+                "Current Player: ".into(),
+                current_player_text,
+                " Active Cell: ".into(),
+                active_cell_row_text,
+                " ".into(),
+                active_cell_col_text,
+            ]))
+            .block(Block::new().borders(Borders::ALL)),
+            outer_layout[1],
+        );
     }
 
     fn handle_events(&mut self) -> Result<()> {
@@ -107,10 +224,11 @@ impl App {
         match (key.modifiers, key.code) {
             (_, KeyCode::Esc | KeyCode::Char('q'))
             | (KeyModifiers::CONTROL, KeyCode::Char('c') | KeyCode::Char('C')) => self.exit(),
-            (_, KeyCode::Left) => self.move_active_cell(Dir::Left),
-            (_, KeyCode::Right) => self.move_active_cell(Dir::Right),
-            (_, KeyCode::Up) => self.move_active_cell(Dir::Up),
-            (_, KeyCode::Down) => self.move_active_cell(Dir::Down),
+            (_, KeyCode::Left) => self.game_state.move_active_cell(Dir::Left),
+            (_, KeyCode::Right) => self.game_state.move_active_cell(Dir::Right),
+            (_, KeyCode::Up) => self.game_state.move_active_cell(Dir::Up),
+            (_, KeyCode::Down) => self.game_state.move_active_cell(Dir::Down),
+            (_, KeyCode::Enter) => self.game_state.toggle_current_player(),
             _ => {}
         }
     }
@@ -118,51 +236,27 @@ impl App {
     fn exit(&mut self) {
         self.exit = true;
     }
-
-    fn move_active_cell(&mut self, direction: Dir) {
-        let next_active_cell: CellPos = (Row::R1, Col::C2);
-        self.active_cell = next_active_cell;
-    }
 }
 
-
-#[derive(Debug, Default)]
-struct Cell {
-    owner: Option<Player>,
-    is_active: bool,
-}
-
-impl Cell {
-    pub fn new() -> Self {
-        Self::default()
-    }
-}
-
-impl Widget for &Cell {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let text = show_player(self.owner.clone());
-
-        let block;
-        if self.is_active {
-            block = Block::bordered().border_set(border::THICK);
-        } else {
-            block = Block::bordered().border_set(border::PLAIN);
-        };
-
-        Paragraph::new(text)
-            .block(block)
-            .centered()
-            .render(area, buf);
-    }
-}
-
+// impl Widget for &Cell<' _> {
+//     fn render(self, area: Rect, buf: &mut Buffer) {
+//         let text = show_player(self.owner.clone());
+//         let block;
+//         if self.is_active {
+//             block = Block::bordered().border_set(border::THICK);
+//         } else {
+//             block = Block::bordered().border_set(border::PLAIN);
+//         };
+//         Paragraph::new(text)
+//             .block(block)
+//             .centered()
+//             .render(area, buf);
+//     }
+// }
 
 impl Widget for &App {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Line::from(" Tic Tac Tui ")
-            .bold()
-            .blue()
-            .centered();
+        let title = Line::from(" Tic Tac Tui ").bold().blue().centered();
         let instructions = Line::from(vec![
             " Decrement ".into(),
             "<Left>".blue().bold(),
@@ -177,28 +271,43 @@ impl Widget for &App {
             .title_bottom(instructions.centered())
             .border_set(border::THICK);
 
-
         // Paragraph::new(game_text)
         //     .centered()
         //     .block(block)
         //     .render(area, buf);
 
-        let col_constraints = (0..3).map(|_| Constraint::Percentage(30));
-        let row_constraints = (0..3).map(|_| Constraint::Percentage(30));
+        let col_constraints = [
+            Constraint::Percentage(30),
+            Constraint::Percentage(30),
+            Constraint::Percentage(30),
+        ];
+        let row_constraints = [
+            Constraint::Percentage(30),
+            Constraint::Percentage(30),
+            Constraint::Percentage(30),
+        ];
         let horizontal = Layout::horizontal(col_constraints).spacing(1);
         let vertical = Layout::vertical(row_constraints);
 
-        let rows = vertical.split(area);
-        let cells = rows.iter().flat_map(|&row| horizontal.split(row).to_vec());
+        let row_rects = vertical.split(area);
 
-        for (i, cell) in cells.enumerate() {
-            let c = Cell::new();
-            c.render(cell, buf)
-            // Paragraph::new(format!("Area {:02}", i + 1))
+        for (r, row_rect) in row_rects.iter().enumerate() {
+            let col_rects = Layout::default()
+                .direction(Direction::Horizontal)
+                .vertical_margin(0)
+                .horizontal_margin(1)
+                .constraints(col_constraints.clone())
+                .split(*row_rect);
+
+            for (c, cell_rect) in col_rects.iter().enumerate() {
+                let cell = self.game_state.cell((r, c));
+
+                // c.render(cell, buf)
+                // Paragraph::new(format!("Area {:02}", i + 1))
                 // .block(Block::bordered())
                 // .render(cell, buf);
+            }
         }
-
     }
 }
 
