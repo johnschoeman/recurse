f98d9776f00d5c6b1264790c9472507973b0a745
John Schoeman <johnschoeman1617@gmail.com>
2025-08-12 13:55:54 -0400

    System to move player

--- space-invaders/src/main.rs
+++ space-invaders/src/main.rs
@@ -1,52 +1,87 @@
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
 
-#[derive(Component)]
-struct Person;
+const PLAYER_SIZE: Vec2 = Vec2::new(50.0, 20.0);
+const GAP_BETWEEN_PLAYER_AND_BOTTOM: f32 = 30.0;
+const PLAYER_SPEED: f32 = 500.0;
+const PLAYER_PADDING: f32 = 10.0;
 
-#[derive(Component)]
-struct Name(String);
+const WINDOW_HEIGHT: f32 = 580.0;
+const WINDOW_WIDTH: f32 = 1000.0;
 
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
-}
+const BACKGROUND_COLOR: Color = Color::srgb(0.9, 0.9, 0.9);
+const PLAYER_COLOR: Color = Color::srgb(0.3, 0.3, 0.7);
+const TEXT_COLOR: Color = Color::srgb(0.5, 0.5, 1.0);
+const SCORE_COLOR: Color = Color::srgb(1.0, 0.5, 0.5);
 
-#[derive(Resource)]
-struct GreetTimer(Timer);
+pub struct SpaceInvaderPlugin;
 
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    // update our timer with the time elapsed since the last update
-    // if that caused the timer to finish, we say hello to everyone
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in &query {
-            println!("hello {}!", name.0);
-        }
-    }
-}
-
-fn update_people(mut query: Query<&mut Name, With<Person>>) {
-    for mut name in &mut query {
-        if name.0 == "Elaina Proctor" {
-            name.0 = "Elaina Hume".to_string();
-            break; // We don't need to change any other names.
-        }
-    }
-}
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
+impl Plugin for SpaceInvaderPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
-        app.add_systems(Startup, add_people);
-        app.add_systems(Update, (update_people, greet_people).chain());
+        app.insert_resource(Score(0));
+        app.add_systems(Startup, setup);
+        app.add_systems(FixedUpdate, move_player);
     }
 }
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(HelloPlugin)
+        .add_plugins(SpaceInvaderPlugin)
         .run();
+}
+
+#[derive(Component)]
+struct Player;
+
+#[derive(Resource, Deref, DerefMut)]
+struct Score(usize);
+
+#[derive(Component, Default)]
+struct Collider;
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
+) {
+    commands.spawn(Camera2d);
+
+    let player_y = WINDOW_HEIGHT / -2.0 + GAP_BETWEEN_PLAYER_AND_BOTTOM;
+
+    commands.spawn((
+        Sprite::from_color(PLAYER_COLOR, Vec2::ONE),
+        Transform {
+            translation: Vec3::new(0.0, player_y, 0.0),
+            scale: PLAYER_SIZE.extend(1.0),
+            ..default()
+        },
+        Player,
+        Collider,
+    ));
+}
+
+fn move_player(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut player_transform: Single<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    let mut direction = 0.0;
+
+    if keyboard_input.pressed(KeyCode::ArrowLeft) {
+        direction -= 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::ArrowRight) {
+        direction += 1.0;
+    }
+
+    let next_player_position =
+        player_transform.translation.x + direction * PLAYER_SPEED * time.delta_secs();
+
+    let left_bound = WINDOW_WIDTH / -2.0;
+    let right_bound = WINDOW_WIDTH / 2.0;
+
+    player_transform.translation.x = next_player_position.clamp(left_bound, right_bound);
 }
